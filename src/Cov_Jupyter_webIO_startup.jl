4466ecc6345c394947e9567889991e5387156e3e
ankit48365 <ankit48365@gmail.com>
2021-09-26 17:47:27 -0400

    f

---
+++ src/Cov_Jupyter_webIO_startup.jl
@@ -0,0 +1,30 @@
+module Covid_Forecast
+
+# Write your package code here.
+using Pkg
+
+Pkg.add("Conda")
+Pkg.add("webio_jupyter_extension")
+Pkg.add("")
+Pkg.add("")
+Pkg.add("")
+
+
+
+#using Conda
+
+using WebIO
+
+ #https://juliagizmos.github.io/WebIO.jl/latest/providers/ijulia/
+
+#using Interact
+#using CSV
+#using DataFrames
+#Conda.pip_interop(true)
+#Conda.pip("install", "webio_jupyter_extension")
+
+#using IJulia
+#notebook()
+
+
+end
--- src/Covid_Forecast.jl
+++ src/Covid_Forecast.jl
@@ -2,15 +2,15 @@ module Covid_Forecast
 
 # Write your package code here.
 #using WebIO
-#using IJulia
+using IJulia
 #using Conda #https://juliagizmos.github.io/WebIO.jl/latest/providers/ijulia/
-using Pluto
+#using Pluto
 #using Interact
 #using CSV
 #using DataFrames
 #Conda.pip_interop(true)
 #Conda.pip("install", "webio_jupyter_extension")
-#notebook()
-Pluto.run()
+notebook()
+#Pluto.run()
 
 end
