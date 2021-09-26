23aaaa179d64597750c72618e2996b717e74c632
ankit48365 <ankit48365@gmail.com>
2021-09-26 17:23:23 -0400

    1

--- src/Covid_Forecast.jl
+++ src/Covid_Forecast.jl
@@ -1,5 +1,16 @@
 module Covid_Forecast
 
 # Write your package code here.
+#using WebIO
+#using IJulia
+#using Conda #https://juliagizmos.github.io/WebIO.jl/latest/providers/ijulia/
+using Pluto
+#using Interact
+#using CSV
+#using DataFrames
+#Conda.pip_interop(true)
+#Conda.pip("install", "webio_jupyter_extension")
+#notebook()
+Pluto.run()
 
 end
